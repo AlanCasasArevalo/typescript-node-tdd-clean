ea156d497594cf3566db5320a34a2b49c0316e73
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-07 19:45:54 +0200

    [FEATURE][ADD ACCOUNT] Cuando todo ha ido bien se devuelve una cuenta creada.

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -10,7 +10,7 @@ export class DbAddAccount implements AddAccount {
 
   async add (account: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(account.password)
-    await this.addAccountRepository.add(Object.assign({}, account, { password: hashedPassword }))
-    return await new Promise(resolve => resolve(null))
+    const accountCreated = await this.addAccountRepository.add(Object.assign({}, account, { password: hashedPassword }))
+    return accountCreated
   }
 }
