4a6debc5866a6ef4a440874c02563b7f0cfeb669
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-10 10:40:35 +0200

    [REFACTOR][ADD ACCOUNT REPOSITORY TEST] Se elimina despues de cada test toda la base de datos.

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,6 +10,11 @@ describe('ACCOUNT MONGO REPOSITORY', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   const makeSut = (): AccountMongoRepository => {
     const sut = new AccountMongoRepository()
     return sut
