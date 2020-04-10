cabca0763ee56de1d8fb5ac1720d93a8bc15a248
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-10 08:35:48 +0200

    [FEATURE][ENCRYPTER] Se crea el mock de bcrypt haciendo que siempre devuelva un hash_value en la promesa Se crea el test para comprobar que dado un valor se devuelve el valor mockeado de dicho valor.

--- src/criptography/crypt-adapter.spec.ts
+++ src/criptography/crypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { CryptAdapter } from './crypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hash_value'))
+  }
+}))
+
 describe('CRYPT ADAPTER', () => {
   test('Should call crypt with correct value', async () => {
     const salt = 10
@@ -8,5 +14,12 @@ describe('CRYPT ADAPTER', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const salt = 10
+    const sut = new CryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash_value')
   })
 })
