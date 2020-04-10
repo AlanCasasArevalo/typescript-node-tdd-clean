9e555be77f1058d42354ad4db22f4cbc525c6f4d
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-10 10:34:02 +0200

    [FEATURE][ADD ACCOUNT REPOSITORY] Creamos la implementacion del una insercion de una nueva cuenta.

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,20 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AddAccountModel } from '../../../../domain/uses-cases/add-account'
+import { AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    // Obtenemos del helper de mongo la coleccion a la que estamos accediendo
+    const accountCollection = MongoHelper.getCollection('accounts')
+    // Metemos en la coleccion el objeto que queremos meter
+    const result = await accountCollection.insertOne(accountData)
+    // Obtenemos de la insercion el resultado en la primera posicion del array nos devuelve el dato insertado correctamente.
+    const account = result.ops[0]
+    // Cambiamos el ._id que nos llega desde mongoDb a .id
+    const { _id, ...accountWithoutId } = account
+    const accountToReturn = Object.assign({}, accountWithoutId, { id: _id })
+    // Devolvemos el resultado de nuestra insercion correcta.
+    return await new Promise(resolve => resolve(accountToReturn))
+  }
+}
