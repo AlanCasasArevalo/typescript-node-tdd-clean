3dc7abaffa6b65de5da389c8204d0ed239304174
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-10 10:35:03 +0200

    [TEST][ADD ACCOUNT REPOSITORY] Hacemos el test de que al hacer una insercion en mongodb de una nueva cuenta dicha cuenta nos devuelve los datos correctos insertados.

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
 
 describe('ACCOUNT MONGO REPOSITORY', () => {
   beforeAll(async () => {
@@ -9,7 +10,18 @@ describe('ACCOUNT MONGO REPOSITORY', () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return an account on success', () => {
-    expect(true).toBeTruthy()
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@gmail.com')
+    expect(account.password).toBe('any_password')
   })
 })
