1f48261184de83436baed3d6c64db3231fcea8a7
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-10 08:39:19 +0200

    [REFACTOR][ENCRYPTER] Se cambia la creacion del metodo en una funcion

--- src/criptography/crypt-adapter.spec.ts
+++ src/criptography/crypt-adapter.spec.ts
@@ -6,19 +6,21 @@ jest.mock('bcrypt', () => ({
     return await new Promise(resolve => resolve('hash_value'))
   }
 }))
+const salt = 10
+const makeSut = (): CryptAdapter => {
+  return new CryptAdapter(salt)
+}
 
 describe('CRYPT ADAPTER', () => {
   test('Should call crypt with correct value', async () => {
-    const salt = 10
-    const sut = new CryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return a hash on success', async () => {
-    const salt = 10
-    const sut = new CryptAdapter(salt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash_value')
   })
