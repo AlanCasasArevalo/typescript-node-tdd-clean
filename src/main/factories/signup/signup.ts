02254b2a190b248fc51b378b1b6817ded4ed4966
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-08-05 07:34:11 +0200

    [REFACTOR][SIGN UP] * Sign up was moved to own folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,15 +1,15 @@
 /** **********************************   Interfaces      ************************************/
-import { Controller } from '../../presentation/protocols'
-import { Encrypter } from '../../data/protocols/encrypter'
-import { AddAccount } from '../../domain/uses-cases/add-account'
-import { AddAccountRepository } from '../../data/protocols/add-account-repository'
+import { Controller } from '../../../presentation/protocols'
+import { Encrypter } from '../../../data/protocols/encrypter'
+import { AddAccount } from '../../../domain/uses-cases/add-account'
+import { AddAccountRepository } from '../../../data/protocols/add-account-repository'
 /** **********************************   Instances      ************************************/
-import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { CryptAdapter } from '../../infra/criptography/crypt-adapter'
-import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-import { LogControllerDecorator } from '../decorators/log'
-import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/LogMongoRepository'
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { CryptAdapter } from '../../../infra/criptography/crypt-adapter'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../../decorators/log'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/LogMongoRepository'
 import { makeSignUpValidation } from './signup-validations'
 
 export const makeSignUpController = (): Controller => {
