f7a7bdcfb329c2f30afb330f82ae458143e9f0fd
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-08-05 07:10:32 +0200

    [TEST][VALIDATION COMPOSITE] Should not return if validation success

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -45,4 +45,12 @@ describe('Validation Composite', () => {
     })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation success', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({
+      field: 'any_value'
+    })
+    expect(error).toBeFalsy()
+  })
 })
